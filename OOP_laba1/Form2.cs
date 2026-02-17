8165602c333bba7f94b2ae8d7c6ca5c828ffdb8e
Roman <roman.peskoff@gmail.com>
2026-02-18 01:20:00 +0300

    finish with form logic

--- OOP_laba1/Form2.cs
+++ OOP_laba1/Form2.cs
@@ -12,6 +12,20 @@ namespace OOP_laba1
         public Form2()
         {
             InitializeComponent();
+            numericUpDown_Balance.Minimum = -1000;
+            numericUpDown_Employee.Minimum = -1000;
+            _instance = new Airport();
+            _instance = new Airport();
+            _instance = new Airport();
+            _instance = new Airport(
+                "Домодедово",
+                "Московская область",
+                150,
+                2000,
+                503232106,
+                4.92,
+                3124
+            );
         }
 
         private void button_Back_Click(object sender, EventArgs e)
@@ -62,17 +76,67 @@ namespace OOP_laba1
 
         private void ParseValues()
         {
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    if (string.IsNullOrWhiteSpace(textBox_Name.Text))
+                        throw new AirportInvalidCastException("Название не может быть пустым.");
+
+                    _instance.Name = textBox_Name.Text;
+                    break;
+                case 1:
+                    if (string.IsNullOrWhiteSpace(textBox_Location.Text))
+                        throw new AirportInvalidCastException("Местоположение не может быть пустым.");
+
+                    _instance.Location = textBox_Name.Text;
+                    break;
+                case 2:
+                    _instance.FlightsPerDay =
+                        (int)numericUpDown_FlightsPerDay.Value < 0
+                            ? throw new AirportInvalidCastException("Значение не может быть отрицательным")
+                            : (int)numericUpDown_FlightsPerDay.Value;
+                    break;
+                case 3:
+                    _instance.TicketsSold = (int)numericUpDown_TicketsSold.Value < 0
+                        ? throw new AirportInvalidCastException("Значение не может быть отрицательным")
+                        : (int)numericUpDown_TicketsSold.Value;
+                    break;
+                case 4:
+                    _instance.Balance = numericUpDown_Balance.Value;
+                    break;
+                case 5:
+                    _instance.Rating = (int)numericUpDown_Rating.Value < 0
+                        ? throw new AirportInvalidCastException("Значение не может быть отрицательным")
+                        : (int)numericUpDown_Rating.Value;
+                    break;
+                case 6:
+                    _instance.EmployeesCount = (int)numericUpDown_Employee.Value < 0
+                        ? throw new AirportInvalidCastException("Значение не может быть отрицательным")
+                        : (int)numericUpDown_Employee.Value;
+                    break;
+            }
+            label_Info.Text = _instance.ToString();
+            if (!isShowedInfo)
+            {
+                button_show.Text = "Скрыть информацию";
+                isShowedInfo = true;
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
             try
             {
-                switch (comboBox.SelectedIndex)
-                {
-                    case 0:
-
-                }
+                ParseValues();
             }
             catch (Exception exception)
             {
-                
+                NativeMessageBox.MessageBox(
+                    IntPtr.Zero,
+                    exception.Message,
+                    "Ошибка",
+                    NativeMessageBox.MB_OK | NativeMessageBox.MB_ICONERROR
+                );
             }
         }
     }
