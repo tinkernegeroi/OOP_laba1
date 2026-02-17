b4555f1ad7388e7384cb6e81556bb15cd20bc138
Roman <roman.peskoff@gmail.com>
2026-02-18 00:51:17 +0300

    add invalidcast exception

---
+++ OOP_laba1/src/exceptions/AirportInvalidCastException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOP_laba1
+{
+    public class AirportInvalidCastException : Exception
+    {
+        public AirportInvalidCastException() 
+            : base("Ошибка приведения типа.") { }
+
+        public AirportInvalidCastException(string message) 
+            : base(message) { }
+    }
+}
