0e6b255c8c63bf93e1f144b0582487c270be8f6b
Roman <roman.peskoff@gmail.com>
2026-02-20 01:11:42 +0300

    add tests

---
+++ AirportTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AirportTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AirportTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ed8e563-be28-4777-95bb-9bebc44f40c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AirportTests/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OOP_laba1;
+
+namespace AirportTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Airport airport;
+
+        [TestMethod]
+        public void ZeroParamConstructor()
+        {
+            airport = new Airport();
+            Assert.AreEqual(0, airport.EmployeesCount);
+        }
+
+        [TestMethod]
+        public void OneParamConstructor()
+        {
+            string name = "Домодедово";
+            airport = new Airport(name);
+            Assert.AreEqual(name, airport.Name);
+        }
+
+        [TestMethod]
+        public void TwoParamConstructor()
+        {
+            string name = "Шереметьево";
+            string location = "Москва";
+            airport = new Airport(name, location);
+
+            Assert.AreEqual(location, airport.Location);
+        }
+
+        [TestMethod]
+        public void AllParamConstructor()
+        {
+            string name = "Домодедово";
+            string location = "Московская область";
+            int flightsPerDay = 150;
+            int ticketsSold = 2000;
+            decimal balance = 503232106;
+            double rating = 4.92;
+            int employeesCount = 3124;
+
+            airport = new Airport(name, location, flightsPerDay, ticketsSold, balance, rating, employeesCount);
+
+            Assert.AreEqual(flightsPerDay, airport.FlightsPerDay);
+            Assert.AreEqual(ticketsSold, airport.TicketsSold);
+            Assert.AreEqual(balance, airport.Balance);
+            Assert.AreEqual(rating, airport.Rating);
+            Assert.AreEqual(employeesCount, airport.EmployeesCount);
+        }
+        [TestMethod]
+        public void ObjectCountTest()
+        {
+            int count = Airport.ObjectsCount;
+            var airport1 = new Airport();
+            var airport2 = new Airport();
+            Assert.AreEqual(count + 2, Airport.ObjectsCount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AirportInvalidCastException))]
+        public void InvalidCastTest()
+        {
+            string name = "Домодедово";
+            string location = "Московская область";
+            int flightsPerDay = 150;
+            int ticketsSold = 2000;
+            decimal balance = -503232106;
+            double rating = 4.92;
+            int employeesCount = 3124;
+            airport = new Airport(name, location, flightsPerDay, ticketsSold, balance, rating, employeesCount);
+            
+        }
+
+        [TestMethod]
+        public void FlightsPerDayHexTest()
+        {
+            string name = "Домодедово";
+            string location = "Московская область";
+            int flightsPerDay = 150;
+            int ticketsSold = 2000;
+            decimal balance = 503232106;
+            double rating = 4.92;
+            int employeesCount = 3124;
+            airport = new Airport(name, location, flightsPerDay, ticketsSold, balance, rating, employeesCount);
+            
+            Assert.AreEqual(flightsPerDay.ToString("X"), airport.GetFlightsPerDayHex());
+        }
+
+    }
+}
